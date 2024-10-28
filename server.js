87e34a5620c0879ccb99dd6fe08c849490ceb8c6
Tirawat07 <tirawat7@hotmail.com>
2024-10-28 15:19:49 +0700

    start authRoute

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const authRoute = require('./routes/authRoute')
 const app = express()
 
+
+app.use('/',authRoute)
 
 const port = process.env.PORT || 8000
 app.listen(port, ()=> console.log('server on ' ,port))
